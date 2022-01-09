30e5466aa59873be4457322a7647b533f6534381
juspinas <38094067+juspinas@users.noreply.github.com>
2022-01-10 11:09:29 +1300

    Made dynamic pages for blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,26 +1,26 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.id}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
     </Layout>
   )
 }
-
+// Had trouble getting node.slug working for the link, kept showing as "undefined". Used node.id instead and changed blog pages to {mdx.id}.js
 export const query = graphql`
   query {
     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
@@ -30,7 +30,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
